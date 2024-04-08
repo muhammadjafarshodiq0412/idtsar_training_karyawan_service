141104845a7fb6a6e27634aad06c15bb8f17dc28
Jafar <mjafar.shodiq@myboost.co>
2024-04-09 00:04:39 +0700

    - add cors config and disable cors

--- src/main/java/com/trainingkaryawan/config/security/SecurityConfig.java
+++ src/main/java/com/trainingkaryawan/config/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -33,7 +34,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(
                         req -> req.requestMatchers("/user-login/**", "/user-register/**")
                                 .permitAll()
